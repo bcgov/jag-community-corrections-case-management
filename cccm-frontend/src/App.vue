74367b954bd17f2a45a169fc7e04d148a62a8f12
echo liang <hhliang66@hotmail.com>
2022-04-13 19:48:47 -0700

    Use router

--- cccm-frontend/src/App.vue
+++ cccm-frontend/src/App.vue
@@ -1,24 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/crnacmp">CRNA-CMP-FORM</RouterLink>
+      </nav>
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <RouterView />
 </template>
 
 <style>
-@import './assets/base.css';
+@import '@/assets/base.css';
 
 #app {
   max-width: 1280px;
@@ -30,6 +34,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 header {
   line-height: 1.5;
+  max-height: 100vh;
 }
 
 .logo {
@@ -48,6 +53,31 @@ a,
   a:hover {
     background-color: hsla(160, 100%, 37%, 0.2);
   }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
 }
 
 @media (min-width: 1024px) {
@@ -76,6 +106,15 @@ a,
 
   .logo {
     margin: 0 2rem 0 0;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
---
+++ cccm-frontend/src/views/crnaCmpForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <formio :src="formUrl" v-on:submit="onSubmitMethod" />
+    </div>
+</template>
+<script>
+    import { Formio } from 'vue-formio';
+
+    export default {
+        data: function() {
+            // Load these from vuex or some other state store system.
+            return {
+                formUrl: "https://hcydsplculhonzk.form.io/crnacmpform"
+            }
+        },
+        components: {
+            formio: Formio
+        },
+        methods: {
+            onSubmitMethod: function(submission) {
+                console.log(submission);
+            }
+        }
+    };
+</script>
