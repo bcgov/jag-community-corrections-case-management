4aabe8a7dc595259495f26c708d780232e238746
TayGov <taylor.clausen@gov.bc.ca>
2022-05-05 13:57:06 -0700

    For code climate failure

---
+++ cccm-backend/cccm-api/src/main/java/ca/bc/gov/open/jag/api/codeclimate/DoingItIncorrectly.java
@@ -0,0 +1,35 @@
+package ca.bc.gov.open.jag.api.codeclimate;
+
+public class DoingItIncorrectly {
+
+    public String Test(String test1,String test2,String test3,String test4,String test5,String test6,String test7,String test8,String test9) {
+
+        if (1==1) {
+            if (1==1) {
+                if (1==1) {
+                    if (1==1) {
+                        if (1==1) {
+                            if (1==1) {
+                                if (1==1) {
+                                    if (1==1) {
+                                        if (1==1) {
+                                            if (1==1) {
+
+                                                String test = "hello";
+
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        return "test";
+    }
+
+}
