1be20605554344e41885c03451c6deca5a3a3aad
echo liang <hhliang66@hotmail.com>
2022-05-11 17:38:33 -0700

    Code updates

--- cccm-frontend/src/components/Header.vue
+++ cccm-frontend/src/components/Header.vue
@@ -3,7 +3,7 @@
   <div class="header">
 		<a href="#"><img src="/src/assets/images/gov_bc_logo.svg" width="150px" border="0" /></a>
 		<span class="headerText textShadow">BC Corrections - {{ locationInfo }}</span>
-		<span class="headerNav textShadow"><a href="#" class="headerA">Dashboard</a> | <a href="#" class="headerA">CRNA/CMP Form</a></span>
+		<span class="headerNav textShadow"><a href="/" class="headerA">Dashboard</a> | <a href="/crnacmp" class="headerA">CRNA/CMP Form</a></span>
 	</div>
 </template>
 
