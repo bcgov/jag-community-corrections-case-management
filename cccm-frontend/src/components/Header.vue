d3d58cd093dde942f9cad226b2e8b095d84dc05a
echo liang <hhliang66@hotmail.com>
2022-05-24 12:13:39 -0700

    Updated img reference

--- cccm-frontend/src/components/Header.vue
+++ cccm-frontend/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <!--Header section-->
   <div class="header">
-    <a href="https://gov.bc.ca"><img src="/src/assets/images/gov_bc_logo.svg" width="150px" border="0" /></a>
+    <a href="https://gov.bc.ca"><img src="@/assets/gov_bc_logo.svg" width="150px" border="0" /></a>
     <span class="headerText textShadow">BC Corrections - {{ locationInfo }}</span>
     <span class="headerNav textShadow"><a href="/" class="headerA">Dashboard</a> | <a href="/crnacmp" class="headerA">CRNA/CMP Form</a></span>
   </div>
