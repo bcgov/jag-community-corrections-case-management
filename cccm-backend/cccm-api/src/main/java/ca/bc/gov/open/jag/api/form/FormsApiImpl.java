d0f1281568fcafdf7086e6e829cf8bb0c9bcf0ae
TayGov <taylor.clausen@gov.bc.ca>
2022-06-01 13:28:02 -0700

    Adding log line

--- cccm-backend/cccm-api/src/main/java/ca/bc/gov/open/jag/api/form/FormsApiImpl.java
+++ cccm-backend/cccm-api/src/main/java/ca/bc/gov/open/jag/api/form/FormsApiImpl.java
@@ -12,6 +12,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @ApplicationScoped
@@ -27,7 +28,7 @@ public class FormsApiImpl implements FormsApi {
     public FormDetails addForm(@Valid FormDetails formDetails) {
 
         logger.warning("Form add not implemented");
-
+        logger.log(Level.INFO, "Record Added {}", formDetails);
         return formDetails;
 
     }
