f1f97d4bd2c6585e49ae503b4d954238e42532c5
echo liang <hhliang66@hotmail.com>
2022-06-06 12:34:49 -0700

    Populated all fields for intervention

--- cccm-frontend/src/components/common/FormioQuestionCombo.vue
+++ cccm-frontend/src/components/common/FormioQuestionCombo.vue
@@ -34,6 +34,18 @@ export default {
       tmpJSONStr = tmpJSONStr.replace('${radiodefaultValue}', this.dataModel.radiodefaultValue);
       tmpJSONStr = tmpJSONStr.replace('${radiokey}', this.dataModel.radiokey);
 
+      // Set intervention needed component
+      tmpJSONStr = tmpJSONStr.replace('${key_itv_needed}', this.dataModel.interventionNeeded.key_yn);
+      tmpJSONStr = tmpJSONStr.replace('${defaultValue_ivNeeded}', this.dataModel.interventionNeeded.defaultValue);
+      tmpJSONStr = tmpJSONStr.replace('${key_itv_details}', "itv_" + this.dataModel.interventionNeeded.key_yn);
+      tmpJSONStr = tmpJSONStr.replace('${key_itv_type}', this.dataModel.interventionNeeded.key_itv_type);
+
+      tmpJSONStr = tmpJSONStr.replace('${key_itv_other}', this.dataModel.interventionNeeded.key_itv_other);
+      tmpJSONStr = tmpJSONStr.replace('${value_itv_other}', this.dataModel.interventionNeeded.value_itv_other);
+
+      tmpJSONStr = tmpJSONStr.replace('${key_itv_description}', this.dataModel.interventionNeeded.key_itv_description);
+      tmpJSONStr = tmpJSONStr.replace('${value_itv_description}', this.dataModel.interventionNeeded.value_itv_description);
+      
       //Set the radio component. 
       let tmpJSON = JSON.parse(tmpJSONStr);   
       let objIndex = tmpJSON.components[0].components.findIndex((obj => obj.type == 'radio'));
@@ -47,10 +59,20 @@ export default {
       objIndex = tmpJSON.components[0].components.findIndex((obj => obj.type == 'textarea'));
       tmpJSON.components[0].components[objIndex].key = this.dataModel.comments.key;
       
-      // Set intervention needed component
-      objIndex = tmpJSON.components[0].components.findIndex((obj => obj.type == 'checkbox'));
-      tmpJSON.components[0].components[objIndex].key = this.dataModel.interventionNeeded.key;
-      tmpJSON.components[0].components[objIndex].defaultValue = this.dataModel.interventionNeeded.defaultValue;
+      // Set intervention needed type DDL
+      objIndex = tmpJSON.components[0].components.findIndex((obj => obj.type == 'well'));
+      tmpJSON.components[0].components[objIndex].components[0].columns[0].components[0].data.values = this.dataModel.interventionNeeded.itv_type_data.values;
+      
+      // build button
+      if (this.dataModel.interventionNeeded.button != null){
+        let btnComponent = JSON.parse("{}");
+        btnComponent.type = "button";
+        btnComponent.action = this.dataModel.interventionNeeded.button.action;
+        btnComponent.key = this.dataModel.interventionNeeded.button.key;
+        btnComponent.label = this.dataModel.interventionNeeded.button.label;
+        btnComponent.theme = this.dataModel.interventionNeeded.button.theme;
+        tmpJSON.components[0].components[objIndex].components[2] = btnComponent;
+      }
 
       //console.log("FormInfoDataEntry: ", tmpJSON);
       this.formJSON = tmpJSON;
