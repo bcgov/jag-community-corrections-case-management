b4cc6e832eca0abcf447a63c54cbb0e53e1c4210
echo liang <hhliang66@hotmail.com>
2022-06-27 12:48:49 -0700

    Working version: portal makes authenticated call to backend APIs

--- cccm-frontend/src/middleware/update-token.ts
+++ cccm-frontend/src/middleware/update-token.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 
-export default async function updateToken(): Promise<string> {
+export default async function (): Promise<string> {
     await Vue.$keycloak.updateToken(70);
-    console.log("middleware keycloak: ", Vue.$keycloak);
+    //console.log("middleware keycloak: ", Vue.$keycloak);
     return Vue.$keycloak.token as string;
 }
--- cccm-frontend/src/services/setupAxioInterceptors.ts
+++ cccm-frontend/src/services/setupAxioInterceptors.ts
@@ -3,21 +3,14 @@ import updateToken from '@/middleware/update-token';
 
 const setup = () => {
   axiosInstance.interceptors.request.use(
-    (config) => {
-      const token = updateToken();
-      console.log("token interceptor: ", token);
+    async config => {
+      const token = await updateToken();
+      //console.log("token interceptor: ", token);
       if (token) {
-        //config.headers.common['Authorization'] = `Bearer ${token}`;
         config.headers["Authorization"] = 'Bearer ' + token;  // for Spring Boot back-end
-        //config.headers["x-access-token"] = token; // for Node.js Express back-end
-      }
+       }
       return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
-
-};
+    });
+  };
 
 export default setup;
