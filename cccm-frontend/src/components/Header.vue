2d8cb52dc721db0df34083e6c0ff73003fbf6934
echo liang <hhliang66@hotmail.com>
2022-07-08 10:28:40 -0700

    Hide the clientProfile menu, updated the sample data

--- cccm-frontend/src/components/Header.vue
+++ cccm-frontend/src/components/Header.vue
@@ -13,7 +13,7 @@
     <span class="headerNav textShadow">
       <a href="/" class="headerA">My Dashboard</a> | 
       <a href="/clientsearch" class="headerA">Client Search</a> |
-      <a href="/clientprofile" class="headerA">Client Profile</a> |
+      <!-- <a href="/clientprofile" class="headerA">Client Profile</a> | -->
       <a href="/crnacmp" class="headerA">CRNA/CMP Form</a>
     </span>
   </div>
