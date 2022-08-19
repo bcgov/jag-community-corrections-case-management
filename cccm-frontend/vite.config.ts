0aa3c68bc99214c3dd3dc12ba14f7caa40c8d8cd
echo liang <hhliang66@hotmail.com>
2022-08-19 14:21:12 -0700

    Attempt to add root path

--- cccm-frontend/vite.config.ts
+++ cccm-frontend/vite.config.ts
@@ -9,6 +9,7 @@ import scriptSetup from 'unplugin-vue2-script-setup/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cccm',
   plugins: [
     vue2({
       jsx: true,
