2a368c47ff8c6a424f34c621ae144a386e91b7a1
TayGov <taylor.clausen@gov.bc.ca>
2022-10-13 14:17:24 -0700

    Cleanup continued

--- cccm-backend/cccm-api/src/main/java/ca/bc/gov/open/jag/api/Keys.java
+++ cccm-backend/cccm-api/src/main/java/ca/bc/gov/open/jag/api/Keys.java
@@ -6,4 +6,12 @@ public class Keys {
 
     public static final String USERNAME_PREFIX = "@idir";
 
+    public static final String SEARCH_TYPE_SOUNDEX = "SOUNDEX";
+
+    public static final String SEARCH_TYPE_ID = "ID";
+
+    public static final String SEARCH_TYPE_PARTIAL = "PARTIAL";
+
+    public static final String SEARCH_TYPE_EXACT = "EXACT";
+
 }
