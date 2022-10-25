e7eb39c05f36aa6788277e221f13f3740193f21f
TayGov <taylor.clausen@gov.bc.ca>
2022-10-25 13:07:54 -0700

    Adding endpoints

--- cccm-backend/cccm-api/src/main/java/ca/bc/gov/open/jag/api/service/ValidationService.java
+++ cccm-backend/cccm-api/src/main/java/ca/bc/gov/open/jag/api/service/ValidationService.java
@@ -1,11 +1,21 @@
 package ca.bc.gov.open.jag.api.service;
 
-import ca.bc.gov.open.jag.api.model.validation.ValidationResult;
+import ca.bc.gov.open.jag.cccm.api.openapi.model.ValidationResult;
 
 public interface ValidationService {
 
+    /**
+     *
+     * @param answers answers to be validated
+     * @return failed validations for crna
+     */
     ValidationResult validateCRNA(String answers);
 
+    /**
+     *
+     * @param answers answers to be validated
+     * @return failed validations for sara
+     */
     ValidationResult validateSARA(String answers);
 
 }
