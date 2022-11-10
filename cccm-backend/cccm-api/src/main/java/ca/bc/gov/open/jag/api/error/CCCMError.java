7cea59a8df76081d158b97ddc129cae0936f70f5
TayGov <taylor.clausen@gov.bc.ca>
2022-11-10 12:40:26 -0800

    Add validation to complete endpoint

--- cccm-backend/cccm-api/src/main/java/ca/bc/gov/open/jag/api/error/CCCMError.java
+++ cccm-backend/cccm-api/src/main/java/ca/bc/gov/open/jag/api/error/CCCMError.java
@@ -1,14 +1,18 @@
 package ca.bc.gov.open.jag.api.error;
 
+import ca.bc.gov.open.jag.cccm.api.openapi.model.ValidationResult;
+
 public class CCCMError {
 
-    public CCCMError(CCCMErrorCode error, String errorMessage) {
+    public CCCMError(CCCMErrorCode error, String errorMessage, ValidationResult validationResult) {
         this.error = error;
         this.errorMessage = errorMessage;
+        this.validationResult = validationResult;
     }
 
     private final CCCMErrorCode error;
     private final String errorMessage;
+    private final ValidationResult validationResult;
 
     public CCCMErrorCode getError() {
         return error;
@@ -18,4 +22,7 @@ public class CCCMError {
         return errorMessage;
     }
 
+    public ValidationResult getValidationResult() {
+        return validationResult;
+    }
 }
--- cccm-backend/cccm-api/src/main/java/ca/bc/gov/open/jag/api/error/CCCMException.java
+++ cccm-backend/cccm-api/src/main/java/ca/bc/gov/open/jag/api/error/CCCMException.java
@@ -1,16 +1,29 @@
 package ca.bc.gov.open.jag.api.error;
 
+import ca.bc.gov.open.jag.cccm.api.openapi.model.ValidationResult;
+
 public class CCCMException extends RuntimeException
 {
     private static final long serialVersionUID = 1L;
 
     private final CCCMErrorCode CCCMErrorCode;
+    private ValidationResult validationResult;
 
     public CCCMException(String message, CCCMErrorCode CCCMErrorCode) {
         super(message);
         this.CCCMErrorCode = CCCMErrorCode;
     }
 
+    public CCCMException(String message, CCCMErrorCode CCCMErrorCode, ValidationResult validationResult) {
+        super(message);
+        this.validationResult = validationResult;
+        this.CCCMErrorCode = CCCMErrorCode;
+    }
+
     public CCCMErrorCode getErrorCode() { return CCCMErrorCode; }
+
+    public ValidationResult getValidationResult() {
+        return validationResult;
+    }
 
 }
