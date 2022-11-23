e154aaed75f271c3c413444c9b65ebd35bb78c9a
echo liang <hhliang66@hotmail.com>
2022-11-23 15:17:56 -0800

    Moved loginUserGroup var into mainStore

--- cccm-frontend/src/stores/store.js
+++ cccm-frontend/src/stores/store.js
@@ -1,4 +1,5 @@
-import {defineStore} from 'pinia';
+import Vue from 'vue'
+import { defineStore } from 'pinia';
 import { useLocalStorage, useSessionStorage } from '@vueuse/core'
 import { async_getUserDefaultLocation, async_getUserLocations } from "@/components/form.api";
 
@@ -8,10 +9,36 @@ export const useStore = defineStore('main', {
         locationCD: useLocalStorage('locationCD', ''),
         locationDescription: useLocalStorage('locationDescription', ''),
         locations: useLocalStorage('locations', []),
+        loginUserGroup: useLocalStorage('loginUserGroup', null)
     }),
 
     // actions
     actions: {
+        // List of realm roles:
+        // client-search	
+        // client-view	
+        // data-view	
+        // default-roles-cccm	
+        // form-add
+        // form-delete	
+        // form-update	
+        // form-view
+        // po-manage
+        getLoginUserGroup() {
+            if (this.loginUserGroup == null) {
+                this.loginUserGroup == ''
+                if (Vue.$keycloak.hasRealmRole('client-search', 'client-view', 
+                'data-view', 'form-add', 'form-delete', 'form-update', 'form-view')) {
+                    this.loginUserGroup = Vue.prototype.$USER_GROUP_PO;
+                }
+                if (Vue.$keycloak.hasRealmRole('po-manage')) {
+                    this.loginUserGroup = Vue.prototype.$USER_GROUP_SUPERVISOR
+                }
+                if (Vue.$keycloak.hasRealmRole('form-override')) {
+                    this.loginUserGroup = Vue.prototype.$USER_GROUP_ADMIN
+                }
+            }
+        },
         clearAll() {
             this.locationCD = '';
             this.locationDescription = '';
@@ -28,13 +55,13 @@ export const useStore = defineStore('main', {
         },
         async getUserLocations() {
             if (this.locations == null || this.locations.length == 0) {
-                console.info("Fetching user locations ...");
+                //console.info("Fetching user locations ...");
                 const [error, response] = await async_getUserLocations();
                 if (error) {
                     console.error(error);
                     return [error];
                 } else {
-                    console.info("Locations fetched: ", response);
+                    //console.info("Locations fetched: ", response);
                     if (response != null && response.items != null) {
                         this.locations = response.items;
                     }
@@ -46,13 +73,13 @@ export const useStore = defineStore('main', {
         async getUserDefaultLocation() {
             //console.info("Attempt to fetch async_getUserDefaultLocation.");
             if (this.locationCD == '') {
-                console.info("Fetching default location ...");
+                //console.info("Fetching default location ...");
                 const [error, response] = await async_getUserDefaultLocation();
                 if (error) {
                     console.error(error);
                     return [error];
                 } else {
-                    console.info("Location fetched: ", response);
+                    //console.info("Location fetched: ", response);
                     if (response != null) {
                         this.locationDescription = response.value;
                         this.locationCD = response.key;
