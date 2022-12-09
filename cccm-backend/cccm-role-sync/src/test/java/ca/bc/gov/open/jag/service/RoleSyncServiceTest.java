160306b84f79731c9ffacad867673b062a803169
TayGov <taylor.clausen@gov.bc.ca>
2022-12-09 08:04:05 -0800

    Implement test

--- cccm-backend/cccm-role-sync/src/test/java/ca/bc/gov/open/jag/service/RoleSyncServiceTest.java
+++ cccm-backend/cccm-role-sync/src/test/java/ca/bc/gov/open/jag/service/RoleSyncServiceTest.java
@@ -1,5 +1,7 @@
 package ca.bc.gov.open.jag.service;
 
+import ca.bc.gov.open.jag.Keys;
+import ca.bc.gov.open.jag.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,10 +14,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.mockito.ArgumentMatchers.any;
 
@@ -51,18 +50,230 @@ public class RoleSyncServiceTest {
     @Test
     public void testProcessAll() {
 
-        Mockito.when(cccmApiServiceMock.getUsers(any())).thenReturn(new ArrayList<>());
+        Mockito.when(cccmApiServiceMock.getUsers(any())).thenReturn(createUsers());
         Mockito.when(usersResourceMock.search(any())).thenReturn(new ArrayList<>());
         Mockito.when(usersResourceMock.list()).thenReturn(new ArrayList<>());
-        Mockito.when(usersResourceMock.get(any())).thenReturn(null);
+        Mockito.doNothing().when(usersResourceMock).create(any());
+        Mockito.when(usersResourceMock.get(any())).thenReturn(new UserResource() {
+            @Override
+            public UserRepresentation toRepresentation() {
+                return null;
+            }
+
+            @Override
+            public void update(UserRepresentation userRepresentation) {
+
+            }
+
+            @Override
+            public void remove() {
+
+            }
+
+            @Override
+            public List<GroupRepresentation> groups() {
+                return createGroups();
+            }
+
+            @Override
+            public List<GroupRepresentation> groups(Integer integer, Integer integer1) {
+                return null;
+            }
+
+            @Override
+            public List<GroupRepresentation> groups(String s, Integer integer, Integer integer1) {
+                return null;
+            }
+
+            @Override
+            public List<GroupRepresentation> groups(Integer integer, Integer integer1, boolean b) {
+                return null;
+            }
+
+            @Override
+            public List<GroupRepresentation> groups(String s, Integer integer, Integer integer1, boolean b) {
+                return null;
+            }
+
+            @Override
+            public Map<String, Long> groupsCount(String s) {
+                return null;
+            }
+
+            @Override
+            public void joinGroup(String s) {
+
+            }
+
+            @Override
+            public void leaveGroup(String s) {
+
+            }
+
+            @Override
+            public void logout() {
+
+            }
+
+            @Override
+            public List<CredentialRepresentation> credentials() {
+                return null;
+            }
+
+            @Override
+            public List<String> getConfiguredUserStorageCredentialTypes() {
+                return null;
+            }
+
+            @Override
+            public void removeCredential(String s) {
+
+            }
+
+            @Override
+            public void setCredentialUserLabel(String s, String s1) {
+
+            }
+
+            @Override
+            public void moveCredentialToFirst(String s) {
+
+            }
+
+            @Override
+            public void moveCredentialAfter(String s, String s1) {
+
+            }
+
+            @Override
+            public void disableCredentialType(List<String> list) {
+
+            }
+
+            @Override
+            public void resetPassword(CredentialRepresentation credentialRepresentation) {
+
+            }
+
+            @Override
+            public void resetPasswordEmail() {
+
+            }
+
+            @Override
+            public void resetPasswordEmail(String s) {
+
+            }
+
+            @Override
+            public void executeActionsEmail(List<String> list) {
+
+            }
+
+            @Override
+            public void executeActionsEmail(List<String> list, Integer integer) {
+
+            }
+
+            @Override
+            public void executeActionsEmail(String s, String s1, Integer integer, List<String> list) {
+
+            }
+
+            @Override
+            public void executeActionsEmail(String s, String s1, List<String> list) {
+
+            }
+
+            @Override
+            public void sendVerifyEmail() {
+
+            }
+
+            @Override
+            public void sendVerifyEmail(String s) {
+
+            }
+
+            @Override
+            public List<UserSessionRepresentation> getUserSessions() {
+                return null;
+            }
+
+            @Override
+            public List<UserSessionRepresentation> getOfflineSessions(String s) {
+                return null;
+            }
+
+            @Override
+            public List<FederatedIdentityRepresentation> getFederatedIdentity() {
+                return null;
+            }
+
+            @Override
+            public Response addFederatedIdentity(String s, FederatedIdentityRepresentation federatedIdentityRepresentation) {
+                return null;
+            }
+
+            @Override
+            public void removeFederatedIdentity(String s) {
+
+            }
+
+            @Override
+            public RoleMappingResource roles() {
+                return null;
+            }
+
+            @Override
+            public List<Map<String, Object>> getConsents() {
+                return null;
+            }
+
+            @Override
+            public void revokeConsent(String s) {
+
+            }
+
+            @Override
+            public Map<String, Object> impersonate() {
+                return null;
+            }
+        });
         Mockito.when(realmMock.users()).thenReturn(usersResourceMock);
         Mockito.when(realmMock.groups()).thenReturn(groupsResourceMock);
-        Mockito.when(groupsResourceMock.groups()).thenReturn(new ArrayList<>());
+        Mockito.when(groupsResourceMock.groups()).thenReturn(createGroups());
         Mockito.when(keycloakMock.realm(any())).thenReturn(realmMock);
-
 
         Assertions.assertDoesNotThrow(() -> sut.syncRoles());
 
+
+    }
+
+    private List<GroupRepresentation> createGroups() {
+
+        GroupRepresentation poGroup = new GroupRepresentation();
+        poGroup.setName(Keys.PO);
+
+        GroupRepresentation supervisorGroup = new GroupRepresentation();
+        supervisorGroup.setName(Keys.SUPERVISOR);
+
+        GroupRepresentation adminGroup = new GroupRepresentation();
+        adminGroup.setName(Keys.ADMIN);
+
+        return Arrays.asList(poGroup, supervisorGroup, adminGroup);
+
+    }
+
+    private List<User> createUsers() {
+
+        User userOne = new User();
+        userOne.setIdirId("TESTONE");
+
+        User userTwo = new User();
+        userTwo.setIdirId("TESTTWO");
+
+        return Arrays.asList(userOne, userTwo);
 
     }
 
