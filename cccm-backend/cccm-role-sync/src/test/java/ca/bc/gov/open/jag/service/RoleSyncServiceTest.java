e2904b27ab804916bb4875399664376eb1639783
TayGov <taylor.clausen@gov.bc.ca>
2022-12-09 06:57:53 -0800

    Refactor adding test

--- cccm-backend/cccm-role-sync/src/test/java/ca/bc/gov/open/jag/service/RoleSyncServiceTest.java
+++ cccm-backend/cccm-role-sync/src/test/java/ca/bc/gov/open/jag/service/RoleSyncServiceTest.java
@@ -1,45 +1,67 @@
 package ca.bc.gov.open.jag.service;
 
-import io.quarkus.test.Mock;
-import io.quarkus.test.junit.mockito.InjectMock;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.resource.RealmResource;
-import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.admin.client.resource.*;
+import org.keycloak.representations.idm.*;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
-import javax.inject.Inject;
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 
-//@QuarkusTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class RoleSyncServiceTest {
 
-    @Inject
-    RoleSyncServiceImpl roleSyncService;
+    RoleSyncServiceImpl sut;
 
-    @InjectMock
+    @Mock
     CCCMApiService cccmApiServiceMock;
 
-    @InjectMock
+    @Mock
     Keycloak keycloakMock;
 
     @Mock
     RealmResource realmMock;
 
     @Mock
+    GroupsResource groupsResourceMock;
+
+    @Mock
     UsersResource usersResourceMock;
 
-    //@Test
+    @BeforeEach
+    public void setup() {
+
+        MockitoAnnotations.openMocks(this);
+
+        sut = new RoleSyncServiceImpl(cccmApiServiceMock, keycloakMock, "REALM", true);
+
+    }
+
+    @Test
     public void testProcessAll() {
 
         Mockito.when(cccmApiServiceMock.getUsers(any())).thenReturn(new ArrayList<>());
         Mockito.when(usersResourceMock.search(any())).thenReturn(new ArrayList<>());
+        Mockito.when(usersResourceMock.list()).thenReturn(new ArrayList<>());
+        Mockito.when(usersResourceMock.get(any())).thenReturn(null);
         Mockito.when(realmMock.users()).thenReturn(usersResourceMock);
+        Mockito.when(realmMock.groups()).thenReturn(groupsResourceMock);
+        Mockito.when(groupsResourceMock.groups()).thenReturn(new ArrayList<>());
         Mockito.when(keycloakMock.realm(any())).thenReturn(realmMock);
 
-        Assertions.assertDoesNotThrow(() -> roleSyncService.syncRoles());
+
+        Assertions.assertDoesNotThrow(() -> sut.syncRoles());
 
 
     }
