ab67c5c4447eaadaf26564ff2c0c3d22712593c4
TayGov <taylor.clausen@gov.bc.ca>
2022-12-12 08:45:07 -0800

    Test

--- cccm-backend/cccm-api/src/test/java/ca/bc/gov/open/jag/api/model/ClientTest.java
+++ cccm-backend/cccm-api/src/test/java/ca/bc/gov/open/jag/api/model/ClientTest.java
@@ -28,6 +28,8 @@ public class ClientTest {
         sut.setCommunityLocation(TEST_STRING);
         sut.setAlias(TEST_STRING);
         sut.setSealed(TEST_STRING);
+        sut.setAddressStatus(TEST_STRING);
+        sut.getAddressType(TEST_STRING)
 
         Assertions.assertEquals(CLIENT_NO, sut.getClientNo());
         Assertions.assertEquals(TEST_STRING, sut.getClientName());
@@ -39,6 +41,8 @@ public class ClientTest {
         Assertions.assertEquals(TEST_STRING, sut.getCommunityLocation());
         Assertions.assertEquals(TEST_STRING, sut.getAlias());
         Assertions.assertEquals(TEST_STRING, sut.getSealed());
+        Assertions.assertEquals(TEST_STRING, sut.getAddressStatus());
+        Assertions.assertEquals(TEST_STRING, sut.getAddressType());
 
     }
 
