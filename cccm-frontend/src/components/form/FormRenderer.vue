3935085a96977feb444dc693819781b7de5b7742
echo liang <hhliang66@hotmail.com>
2022-12-13 13:04:22 -0800

    Update the class name 'crna-cmp-form' to 'rna-cmp-form'

--- cccm-frontend/src/components/form/FormRenderer.vue
+++ cccm-frontend/src/components/form/FormRenderer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div data-app class="main crna-cmp-form">
+  <div data-app class="main rna-cmp-form">
     <!-- Form delete modal dialog section-->
     <div>
       <!-- Form delete modal dialog-->
