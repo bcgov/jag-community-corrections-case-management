b63ad7ea9d56f5054207f8cfdb8529cdcdae000e
echo liang <hhliang66@hotmail.com>
2023-02-06 16:09:13 -0800

    Added the missing code

--- cccm-frontend/src/components/CmpForm.vue
+++ cccm-frontend/src/components/CmpForm.vue
@@ -175,7 +175,6 @@ export default {
       if (error) {
         console.error("Failed getting client form details", error);
       } else {
-        // console.log("Form details: ", response);
         this.private_getTabs(response);
       }
       this.formKey++;
@@ -211,49 +210,49 @@ export default {
 
         // show the 'add STABLE' btn 
         if (!this.isFormReadonly) {
-          this.items.push({ tab: '', key: 0, id: this.CONST_CREATE_BTN_STABLE,  formId: '', relatedClientFormId: '', readonly: false, locked: false});
+          this.items.push({ tab: '', key: 0, id: this.CONST_CREATE_BTN_STABLE,  formId: '', relatedClientFormId: '', readonly: false, locked: false, createdByIdir: '' });
         }
       } else
       // if formType is 'ACUTE', only show ACUTE tab
       if (this.formType === this.$CONST_FORMTYPE_ACUTE) {
-        this.items.push({ tab: 'ACUTE', key: 0, id: this.$CONST_FORMTYPE_ACUTE, formId: this.formId, relatedClientFormId: null, readonly: this.isFormReadonly, locked: response.locked });
+        this.items.push({ tab: 'ACUTE', key: 0, id: this.$CONST_FORMTYPE_ACUTE, formId: this.formId, relatedClientFormId: null, readonly: this.isFormReadonly, locked: response.locked, createdByIdir: response.createdByIdir });
         this.current_tab = 'tab-ACUTE';
         if (!this.relatedClientFormId) {
           // show the 'add smo-overall' btn if the acute form hasn't linked with smo-overall
           if (!this.isFormReadonly) {
-            this.items.push({ tab: '', key: 0, id: this.CONST_CREATE_BTN_OVERALL,  formId: '', relatedClientFormId: '', readonly: false, locked: false});
+            this.items.push({ tab: '', key: 0, id: this.CONST_CREATE_BTN_OVERALL,  formId: '', relatedClientFormId: '', readonly: false, locked: false, createdByIdir: '' });
           }
         } else {
           // otherwise, show overall tab
-          this.items.push({ tab: 'SMO-OVERALL', key: 0, id: this.$CONST_FORMTYPE_SO_OVERALL, formId: this.relatedClientFormId, relatedClientFormId: this.formId, readonly: false, locked: false });
+          this.items.push({ tab: 'SMO-OVERALL', key: 0, id: this.$CONST_FORMTYPE_SO_OVERALL, formId: this.relatedClientFormId, relatedClientFormId: this.formId, readonly: false, locked: false, createdByIdir: '' });
         }
       } else
       // if formType is 'STAT99R', only show STAT99R tab
       if (this.formType === this.$CONST_FORMTYPE_STAT99R) {
-        this.items.push({ tab: 'STAT99R', key: 0, id: this.$CONST_FORMTYPE_STAT99R, formId: this.formId, relatedClientFormId: null, readonly: this.isFormReadonly, locked: response.locked });
+        this.items.push({ tab: 'STAT99R', key: 0, id: this.$CONST_FORMTYPE_STAT99R, formId: this.formId, relatedClientFormId: null, readonly: this.isFormReadonly, locked: response.locked, createdByIdir: response.createdByIdir });
         this.current_tab = 'tab-STAT99R';
         if (!this.relatedClientFormId) {
           // show the 'add overall' btn if the acute form hasn't linked with overall
           if (!this.isFormReadonly) {
-            this.items.push({ tab: '', key: 0, id: this.CONST_CREATE_BTN_OVERALL,  formId: '', relatedClientFormId: '', readonly: false, locked: false});
+            this.items.push({ tab: '', key: 0, id: this.CONST_CREATE_BTN_OVERALL,  formId: '', relatedClientFormId: '', readonly: false, locked: false, createdByIdir: '' });
           }
         } else {
           // otherwise, show overall tab
-          this.items.push({ tab: 'SMO-OVERALL', key: 0, id: this.$CONST_FORMTYPE_SO_OVERALL, formId: this.relatedClientFormId, relatedClientFormId: this.formId, readonly: false, locked: false });
+          this.items.push({ tab: 'SMO-OVERALL', key: 0, id: this.$CONST_FORMTYPE_SO_OVERALL, formId: this.relatedClientFormId, relatedClientFormId: this.formId, readonly: false, locked: false, createdByIdir: '' });
         }
       } else
       // if formType is 'SMO-OVERALL', only show Overall tab
       if (this.formType === this.$CONST_FORMTYPE_SO_OVERALL) {
-        this.items.push({ tab: 'SMO-OVERALL', key: 0, id: this.$CONST_FORMTYPE_SO_OVERALL, formId: this.formId, relatedClientFormId: null, readonly: this.isFormReadonly, locked: response.locked });
+        this.items.push({ tab: 'SMO-OVERALL', key: 0, id: this.$CONST_FORMTYPE_SO_OVERALL, formId: this.formId, relatedClientFormId: null, readonly: this.isFormReadonly, locked: response.locked, createdByIdir: response.createdByIdir });
         this.current_tab = 'tab-OVERALL';
       } else
       // if formType is 'STABLE', only show STABLE tab
       if (this.formType === this.$CONST_FORMTYPE_STABLE) {
-        this.items.push({ tab: 'STABLE-CMP', key: 0, id: this.$CONST_FORMTYPE_STABLE, formId: this.formId, relatedClientFormId: null, readonly: this.isFormReadonly, locked: response.locked });
+        this.items.push({ tab: 'STABLE-CMP', key: 0, id: this.$CONST_FORMTYPE_STABLE, formId: this.formId, relatedClientFormId: null, readonly: this.isFormReadonly, locked: response.locked, createdByIdir: response.createdByIdir });
         this.current_tab = 'tab-STABLE';
       } else {
         // unsupported formTypes
-        this.items.push({ tab: this.formType, key: 0, id: this.formId, formId: this.formId, relatedClientFormId: null, readonly: this.isFormReadonly, locked: response.locked });
+        this.items.push({ tab: this.formType, key: 0, id: this.formId, formId: this.formId, relatedClientFormId: null, readonly: this.isFormReadonly, locked: response.locked, createdByIdir: response.createdByIdir });
         this.current_tab = 'tab-' + this.formType;
       }
     },
