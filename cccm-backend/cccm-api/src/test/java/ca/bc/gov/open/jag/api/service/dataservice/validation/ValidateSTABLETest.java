6897f6a392f6240cdddfecef11e70f52b04f8b44
TayGov <taylor.clausen@gov.bc.ca>
2023-02-13 08:34:03 -0800

    Adding stable validation

--- cccm-backend/cccm-api/src/test/java/ca/bc/gov/open/jag/api/service/dataservice/validation/ValidateSTABLETest.java
+++ cccm-backend/cccm-api/src/test/java/ca/bc/gov/open/jag/api/service/dataservice/validation/ValidateSTABLETest.java
@@ -35,7 +35,7 @@ public class ValidateSTABLETest {
 
         ValidationResult result = sut.validateStable(DATA_ONE);
 
-        Assertions.assertEquals(10, result.getErrors().size());
+        Assertions.assertEquals(17, result.getErrors().size());
 
     }
 
@@ -47,7 +47,7 @@ public class ValidateSTABLETest {
 
         ValidationResult result = sut.validateStable("{}");
 
-        Assertions.assertEquals(11, result.getErrors().size());
+        Assertions.assertEquals(17, result.getErrors().size());
 
     }
 
@@ -59,7 +59,7 @@ public class ValidateSTABLETest {
 
         ValidationResult result = sut.validateStable("");
 
-        Assertions.assertEquals(11, result.getErrors().size());
+        Assertions.assertEquals(17, result.getErrors().size());
 
     }
 
