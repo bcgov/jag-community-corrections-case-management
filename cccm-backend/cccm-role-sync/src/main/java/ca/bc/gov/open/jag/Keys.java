83b40fad8f9cd8ad78c86ba75317ff542cc83865
TayGov <taylor.clausen@gov.bc.ca>
2023-03-23 10:43:09 -0700

    Add groups and update test

--- cccm-backend/cccm-role-sync/src/main/java/ca/bc/gov/open/jag/Keys.java
+++ cccm-backend/cccm-role-sync/src/main/java/ca/bc/gov/open/jag/Keys.java
@@ -7,6 +7,8 @@ public class Keys {
     public static String PO = "cccm-users";
     public static String SUPERVISOR = "cccm-supervisors";
     public static String ADMIN = "cccm-admin";
+    public static String RESEARCHER = "cccm-researcher";
+    public static String ITRP = "cccm-itrp";
     public static String ORACLE_ID = "oracleId";
     public static String IDIR_IDP = "idir";
     public static String SSO_IDIR_USERNAME_KEY = "idir_username";
