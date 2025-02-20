48ab70fd81689e8d051984eb547098dfccb7279d
TayGov <taylor.clausen@gov.bc.ca>
2025-02-20 07:46:03 -0800

    Add additional logging for debugging

--- cccm-backend/cccm-role-sync/src/main/java/ca/bc/gov/open/jag/service/RoleSyncServiceImpl.java
+++ cccm-backend/cccm-role-sync/src/main/java/ca/bc/gov/open/jag/service/RoleSyncServiceImpl.java
@@ -66,10 +66,11 @@ public class RoleSyncServiceImpl implements RoleSyncService {
         String processingGroup = roleGroupEnumToKeycloakGroup(roleGroup);
         GroupRepresentation representation = keycloak.realm(realm).groups().groups().stream().filter(groupRepresentation -> groupRepresentation.getName().equals(processingGroup)).findFirst().get();
         List<UserRepresentation> users = keycloak.realm(realm).users().list().stream().filter(user -> userInGroup(user, processingGroup)).collect(Collectors.toList());
+        logger.info("Current user count {}", users.size());
         for (User user: dbUsers.stream().filter(user -> StringUtils.isNoneBlank(user.getIdirId())).collect(Collectors.toList())) {
             logger.info("processing user {}", user.getIdirId());
             users.removeIf(remUser -> remUser.getUsername().equals(StringUtils.lowerCase(user.getIdirId())));
-
+            logger.info("Current user count after removal {}", users.size());
             Optional<UserRepresentation> keyCloakUser = keycloak.realm(realm)
                     .users()
                     .searchByUsername(StringUtils.lowerCase(user.getIdirId()), true)
